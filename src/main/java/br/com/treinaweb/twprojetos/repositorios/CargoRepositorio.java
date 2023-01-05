cc082c48405311583f3dc94d365696ca9d0aad89
Felippe <nascimento.conceito@gmail.com>
2023-01-04 23:49:27 -0300

    criação da entidade e repositorio cargo

---
+++ src/main/java/br/com/treinaweb/twprojetos/repositorios/CargoRepositorio.java
@@ -0,0 +1,11 @@
+package br.com.treinaweb.twprojetos.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.treinaweb.twprojetos.entidades.Cargo;
+
+public interface CargoRepositorio extends JpaRepository<Cargo, Long> {
+    
+    
+
+}
