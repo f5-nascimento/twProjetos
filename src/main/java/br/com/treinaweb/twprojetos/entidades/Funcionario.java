1f97a8bb5c1524c9178884fc115bc98689a9ea01
Felippe <nascimento.conceito@gmail.com>
2023-01-05 01:16:12 -0300

    criação o relacionamento funcionario cargo

--- src/main/java/br/com/treinaweb/twprojetos/entidades/Funcionario.java
+++ src/main/java/br/com/treinaweb/twprojetos/entidades/Funcionario.java
@@ -3,6 +3,9 @@ package br.com.treinaweb.twprojetos.entidades;
 import java.time.LocalDate;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 
@@ -16,6 +19,10 @@ public class Funcionario extends Pessoa {
     @Column(name = "data_demissao")
     @DateTimeFormat(iso = ISO.DATE)
     private LocalDate dataDemissao;
+
+    @ManyToOne
+    @JoinColumn(name = "cargo_id_fk", nullable = false)
+    private Cargo cargo;
 
     public LocalDate getDataAdmissao() {
         return dataAdmissao;
@@ -31,6 +38,14 @@ public class Funcionario extends Pessoa {
 
     public void setDataDemissao(LocalDate dataDemissao) {
         this.dataDemissao = dataDemissao;
+    }
+
+    public Cargo getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(Cargo cargo) {
+        this.cargo = cargo;
     }  
     
     
